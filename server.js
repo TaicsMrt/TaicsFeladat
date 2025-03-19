a9c338879bcee1bb92bb9b33a875aa75321ca454
TaicsMrt <taimar487@hengersor.hu>
2025-03-19 11:58:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,25 @@ const mysql = require("mysql");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    port: "3306",
+    password: "",
+    database: "felveteli"
+});
+
+db.connect((err) => {
+    console.log('Csatlakozva az adatbázishoz');
+});
+
+app.get("/", (req, res) => {
+    res.send("Működik a szerverem!");
+});
+
+app.listen(3000, () => {
+    console.log('Fut a szerver!');
+});
